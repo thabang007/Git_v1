ffe8b5d04dd809346a087a25b5791f8f6a3b305e
thabang007 <thabang.mautjane@absa.africa>
2023-04-04 10:10:06 +0200

    Revert "print 3 times"
    This reverts commit 3327df560e65b492ea320e711b01c6b8beefe21b.

--- printer.py
+++ printer.py
@@ -1,11 +1,11 @@
 def print_twice(input: str):
     str = input
-    print(str*3)
+    print(str*2)
 def print_reversed (input: str):
     text = input[::-1]
     print(text)
 def print_header():
-    print("Thanks, now I'll print your string reversed, and twice")
+      print("Thanks, now I'll print your string reversed, and twice")
 if __name__ == "__main__":
     s = input()
     print_header()
